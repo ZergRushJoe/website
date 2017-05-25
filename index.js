0cad90bdbbb1b11aeb6b7c97e447ec31abd9d4ae
Joseph Karp <jkarp@supind.com>
2017-05-25 15:37:09 -0400

    even more setup

--- index.js
+++ index.js
@@ -2,3 +2,18 @@
  * Created by jkarp on 5/25/2017.
  */
 
+const express = require('express');
+const app = express();
+const path = require('path');
+
+//settings
+app.use('/public', express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'pug');
+
+
+app.get('/', function (req, res)
+{
+    res.render('body');
+});
+
+app.listen(80);
