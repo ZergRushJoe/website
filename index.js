c23aca5e27719027b306f8007cda9b492dc31054
Joseph Karp <joe12345hd@yahoo.com>
2017-06-22 19:19:11 -0400

    fixing

--- index.js
+++ index.js
@@ -9,16 +9,12 @@ const path = require('path');
 //settings
 app.use('/public', express.static(path.join(__dirname, 'public')));
 
-<<<<<<< HEAD
-
 
 app.get('/', function (req, res,next)
-=======
-app.get('/', function (req, res)
->>>>>>> 77a2f22f3100d7e58b1c6073c47530149038ddb4
 {
-    
+
 });
+
 app.use(function(err,req,res,next)
 {
     if(err)
