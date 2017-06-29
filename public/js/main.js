675d420caaeaea5fd2d9d3480bcede487cef0867
Joseph Karp <joe12345hd@yahoo.com>
2017-06-29 11:00:49 -0400

    change to trigger on hash change
    Made the code work on hash change inplace of click

--- public/js/main.js
+++ public/js/main.js
@@ -9,4 +9,25 @@ for(let i = 0; i < mainNavList.length;i++)
     let span = mainNavList[i].children[1];
     let size = parseInt(span.offsetWidth);
     span.style.marginLeft = ((110-size)/2) +'px';
+    mainNavList[i].addEventListener('click',selected,false)
+}
+
+window.onhashchange = function()
+{
+    document.getElementsByClassName('selected')[0].classList.remove('selected');
+
+    for(let i = 0; i < mainNavList.length;++i)
+    {
+        if('#'+mainNavList[i].children[1].innerText == window.location.hash)
+        {
+            mainNavList[i].classList.add('selected');
+        }
+
+    }
+
+
+};
+function selected(e)
+{
+    window.location.hash = this.children[1].innerText;
 }
